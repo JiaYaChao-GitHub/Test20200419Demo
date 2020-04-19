723cd5832ce9dee8b4e08bbc9eeb6c2021bcc77b
jycgithub <1141716390@qq.com>
2020-04-19 15:51:57 +0800

    新的java的Test类

---
+++ src/main/java/com/rj/bd/Test.java
@@ -0,0 +1,14 @@
+package com.rj.bd;
+
+import java.util.Date;
+
+/**
+ * @author JiaYaChao
+ * @desc
+ * @time 2020-04-19 15:46
+ */
+public class Test {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("date:" + new Date().toString());
+    }
+}
